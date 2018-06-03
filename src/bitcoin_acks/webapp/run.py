b61c7b8a0585b71c221d5ac148bbd23a5d6d83d4
Pierre Rochard <pierre@rochard.org>
2018-06-03 09:37:12 -0400

    setup basic flask oauth github integration

--- src/bitcoin_acks/webapp/run.py
+++ src/bitcoin_acks/webapp/run.py
@@ -1,5 +1,9 @@
-from flask import Flask, request, Response
+import os
+
+from flask import Flask, request, redirect, url_for, Response
 from flask_admin import Admin
+from flask_admin.menu import MenuLink
+from flask_dance.contrib.github import make_github_blueprint, github
 
 from bitcoin_acks.database.session import session_scope
 from bitcoin_acks.models import PullRequests, Logs
@@ -37,9 +41,19 @@ def create_app(config_object: str):
                       url='/',
                       index_view=PullRequestsModelView(PullRequests, session))
 
+
     @app.route('/robots.txt')
     def robots_txt():
         return Response('User-agent: *\nDisallow: /\n')
+
+    blueprint = make_github_blueprint(
+        client_id=os.environ['GITHUB_OAUTH_CLIENT_ID'],
+        client_secret=os.environ['GITHUB_OAUTH_CLIENT_SECRET'],
+        scope='user:email'
+    )
+    app.register_blueprint(blueprint, url_prefix='/login')
+
+    admin.add_link(MenuLink(name='Login', endpoint='github.login'))
 
     return app
 
